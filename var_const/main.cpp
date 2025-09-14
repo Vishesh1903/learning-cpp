8c922306df28b16d0931272182adc1d338732c32
Vishesh Upreti <visheshupreti32@gmail.com>
2025-09-15 01:17:48 +0530

    learned about variables

---
+++ var_const/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Variables and Constants in C++" << std::endl;
+    int a;
+    a = 10;
+    std::cout << "The value of a is:" << std::endl;
+    std::cout << a << std::endl;
+
+    return 0;
+}
