31bf1e109023979917459ce8020318f305f1b287
Vishesh Upreti <visheshupreti32@gmail.com>
2025-09-17 00:40:43 +0530

    practicing: cin command

---
+++ data_types/primitives/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+
+    string first_name;
+    int age = -1;
+    cin >> first_name >> age;
+    cout << first_name << age;
+    return 0;
+}
