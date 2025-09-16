41f3e80f1b434f5546b16eccc096e593ee7df0b3
Vishesh Upreti <visheshupreti32@gmail.com>
2025-09-16 21:59:38 +0530

    learning: data types

---
+++ data_types/primitives/primitves.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int tea_leaves = 50.93232;
+    float temperature = 85.555555555;
+    double price = 29.9999989898989;
+    char rating = 'a';
+    bool is_tea_ready = false;
+
+    cout << "tea_leaves:" << tea_leaves << endl;
+    cout << "temperature:" << temperature << endl;
+    cout << "price:" << price << endl;
+    cout << "rating:" << rating << endl;
+    cout << "is_tea_ready:" << is_tea_ready << endl;
+
+    return 0;
+}
